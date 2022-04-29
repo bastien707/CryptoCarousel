f4f5f7a96d8bd6408501b4b18a772e6611fa693b
bastien707 <bastien.facqueur@efrei.net>
2022-04-29 12:32:54 -0700

    home page added

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,30 +1,27 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="topnav">
+    <h2>🎠 CryptoCarousel</h2>
+  </div>
+  <section id="contentS1">
+    <h1 class="gradient-text">
+      <span>Welcome to</span>
+      <span class="highlight"> CryptoCarousel </span>
+      <span>a place to find more on the </span>
+      <span class="highlight"> blockchain. </span>
+    </h1>
+    <p class="p-desc">
+      Get up to date with the blockchain by exploring any addresses and keeping
+      an eye on your favorite coin 👀
+    </p>
+
+    <a class="start-ref" href="#contentS2">
+      <button class="start-button">Start now</button>
+    </a>
+  </section>
+  <section id="contentS2"></section>
+  <router-view />
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+@import "@/assets/styles/app.css";
 </style>
