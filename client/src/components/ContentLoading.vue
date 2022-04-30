c14f9ced8f8341add7122d2b1f6110d94dc3a042
bastien707 <bastien.facqueur@efrei.net>
2022-04-30 13:41:45 -0700

    new component ContentLoader created

---
+++ client/src/components/ContentLoading.vue
@@ -0,0 +1,19 @@
+<template>
+  <ContentLoader
+    viewBox="0 0 450 400">
+    <rect x="125" y="40" rx="4" ry="4" width="200" height="20" />
+    <rect x="30" y="304" rx="4" ry="4" width="270" height="10" />
+    <rect x="30" y="323" rx="3" ry="3" width="119" height="6" />
+    <rect x="30" y="77" rx="15" ry="15" width="388" height="217" />
+  </ContentLoader>
+</template>
+
+<script>
+import { ContentLoader } from 'vue-content-loader';
+
+export default {
+  components: {
+    ContentLoader,
+  },
+};
+</script>
--- client/src/components/CryptoCard.vue
+++ client/src/components/CryptoCard.vue
@@ -1,29 +1,46 @@
 <template>
-  <div class='card'>
-    <h2>{{name}}</h2>
-    <br>
-    <h2>{{price}}$</h2>
+  <div class='card-container'>
+    <div class='card-data-loading' v-if="loading === true">
+      <div >
+        <ContentLoading />
+      </div>
+    </div>
+    <div class='card-data' v-else>
+        <h2>{{name}}</h2>
+        <h2>{{price}}$</h2>
+      </div>
   </div>
 </template>
 
 <script>
+import ContentLoading from '@/components/ContentLoading.vue';
+
 export default {
   name: 'CryptoCard',
   props: {
     name: String,
     price: String,
+    loading: Boolean,
+  },
+  components: {
+    ContentLoading,
   },
 };
 </script>
 
 <style scoped>
-    .card {
+    .card-container {
+        width: 250px;
+        height: 250px;
+        margin: 40px 40px;
+        background-color: rgb(255, 255, 255);
+        border-radius: 20px;
+    }
+
+    .card-data {
+        padding:10%;
+        text-align: center;
         display: flex;
         flex-flow: column wrap;
-        text-align: center;
-        margin: auto;
-        padding: 10% 7%;
-        background-color: aliceblue;
-        border-radius: 10px;
     }
 </style>
