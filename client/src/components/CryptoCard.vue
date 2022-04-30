fce19040c9ef79dce4eb34dfef02cb9d05737b34
bastien707 <bastien.facqueur@efrei.net>
2022-04-29 17:21:51 -0700

    component cryptocard created

---
+++ client/src/components/CryptoCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class='card'>
+    <h2>{{name}}</h2>
+    <br>
+    <h2>{{price}}$</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CryptoCard',
+  props: {
+    name: String,
+    price: String,
+  },
+};
+</script>
+
+<style scoped>
+    .card {
+        display: flex;
+        flex-flow: column wrap;
+        text-align: center;
+        margin: auto;
+        padding: 10% 7%;
+        background-color: aliceblue;
+        border-radius: 10px;
+    }
+</style>
