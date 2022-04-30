77deff559668fb21a59f26f3d01c70c80c33f96d
bastien707 <bastien.facqueur@efrei.net>
2022-04-30 13:42:06 -0700

    new cards added

--- client/src/App.vue
+++ client/src/App.vue
@@ -15,14 +15,21 @@
     </p>
 
     <a class="start-ref" href="#contentS2">
-      <button class="start-button">Start now</button>
+      <button class="start-button">Explore</button>
     </a>
   </section>
   <section id="contentS2">
-    <CryptoCard :price="BTC" name="Bitcoin" />
-    <CryptoCard :price="ETH" name="Ethereum"/>
-    <CryptoCard />
-    <CryptoCard />
+    <h1 class=CS2Title>Today's Cryptocurrency Prices</h1>
+    <div class=card-container>
+      <CryptoCard :price="price[0].BTC" name="Bitcoin" :loading="loadingAPI[0].BTC"/>
+      <CryptoCard :price="price[1].ETH" name="Ethereum" :loading="loadingAPI[1].ETH"/>
+      <CryptoCard :price="price[2].MON" name="Monero" :loading="loadingAPI[2].MON"/>
+      <CryptoCard />
+      <CryptoCard />
+      <CryptoCard />
+      <CryptoCard />
+      <CryptoCard />
+    </div>
   </section>
 </template>
 
@@ -37,22 +44,53 @@ export default {
 
   data() {
     return {
-      BTC: null,
+      loadingAPI: [{ BTC: true }, { ETH: true }, { MON: true }],
+      price: [{ BTC: null }, { ETH: null }, { MON: true }],
       ETH: null,
     };
   },
   mounted() {
-    const connectionBTC = new WebSocket('wss://ws.coincap.io/prices?assets=bitcoin');
-    let parsedResponseBTC = null;
-    connectionBTC.onmessage = (eventBTC) => {
-      parsedResponseBTC = JSON.parse(eventBTC.data);
-      this.BTC = parsedResponseBTC.bitcoin;
+    const connectionBTC = new WebSocket('wss://ws.coincap.io/prices?assets=bitcoin,ethereum,monero');
+    let parsedResponse = null;
+    const temp = [{ BTC: null }, { ETH: null }, { MON: null }];
+    connectionBTC.onmessage = (event) => {
+      parsedResponse = JSON.parse(event.data);
+      this.price[0].BTC = parsedResponse.bitcoin;
+      if (typeof this.price[0].BTC === 'string') {
+        temp[0].BTC = this.price[0].BTC;
+      } else if (temp[0].BTC === null) {
+        this.loadingAPI[0].BTC = true;
+      } else {
+        this.price[0].BTC = temp[0].BTC;
+        this.loadingAPI[0].BTC = false;
+      }
+
+      this.price[1].ETH = parsedResponse.ethereum;
+      if (typeof this.price[1].ETH === 'string') {
+        temp[1].ETH = this.price[1].ETH;
+      } else if (temp[1].ETH === null) {
+        this.loadingAPI[1].ETH = true;
+      } else {
+        this.price[1].ETH = temp[1].ETH;
+        this.loadingAPI[1].ETH = false;
+      }
+
+      this.price[2].MON = parsedResponse.monero;
+      if (typeof this.price[2].MON === 'string') {
+        temp[2].MON = this.price[2].MON;
+      } else if (temp[2].MON === null) {
+        this.loadingAPI[2].MON = true;
+      } else {
+        this.price[2].MON = temp[2].MON;
+        this.loadingAPI[2].MON = false;
+      }
     };
     const connectionETH = new WebSocket('wss://ws.coincap.io/prices?assets=ethereum');
     let parsedResponseETH = null;
     connectionETH.onmessage = (eventETH) => {
       parsedResponseETH = JSON.parse(eventETH.data);
       this.ETH = parsedResponseETH.ethereum;
+      this.loadingAPI[1].ETH = false;
     };
   },
 };
