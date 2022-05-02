09fddc5cf604542bf24e4c43bc6388f51e99d147
bastien707 <bastien.facqueur@efrei.net>
2022-05-01 21:15:46 -0700

    component for navigation bar

---
+++ client/src/components/TopNav.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="topnav">
+    <nav>
+      <h2>🎠 CryptoCarousel</h2>
+      <router-link to="/">Home</router-link>
+      <router-link to="/Address">Address</router-link>
+    </nav>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'TopNav',
+};
+</script>
+
+<style scoped>
+  .topnav{
+    display: flex;
+    height: 5vh;
+    background-color: rgb(255, 255, 255);
+    box-shadow:0 10px 20px -10px #cdcdcd;
+    padding: 2%;
+}
+</style>
