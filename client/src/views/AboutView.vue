f309c7ed7cbe84009cbdc762ef4b5ad70632878c
bastien707 <bastien.facqueur@efrei.net>
2022-05-01 23:39:47 -0700

    new About page

---
+++ client/src/views/AboutView.vue
@@ -0,0 +1,19 @@
+<template>
+    <TopNav />
+    <h1>About</h1>
+    <FooterView />
+</template>
+
+<script>
+import TopNav from '@/components/TopNav.vue';
+import FooterView from '@/components/FooterView.vue';
+
+export default {
+  name: 'AboutView',
+  components: {
+    TopNav,
+    FooterView,
+  },
+};
+
+</script>
