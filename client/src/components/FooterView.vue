ca72401d20d0ca4cd9b19c5305381c0b132d8f07
bastien707 <bastien.facqueur@efrei.net>
2022-05-01 23:38:31 -0700

    new Footer Component

---
+++ client/src/components/FooterView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="footer">
+    <h2>© Copyright 2022</h2>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'FooterView',
+};
+</script>
+
+<style scoped>
+
+  .footer{
+    display: flex;
+    align-content: center;
+    flex-flow: row wrap;
+    justify-content: space-between;
+    height: 5vh;
+    background-color: rgb(0, 0, 0);
+    height: 6em;
+    padding: 0 2%;
+}
+
+h2  {
+  color: white;
+}
+</style>
