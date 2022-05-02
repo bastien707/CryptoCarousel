8232ef301ba98a4750c507f54c24b86406b90201
bastien707 <bastien.facqueur@efrei.net>
2022-05-01 23:39:07 -0700

    improve routing and style

--- client/src/components/TopNav.vue
+++ client/src/components/TopNav.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="topnav">
-    <nav>
-      <h2>🎠 CryptoCarousel</h2>
-      <router-link to="/">Home</router-link>
-      <router-link to="/Address">Address</router-link>
-    </nav>
+    <h2>🎠 CryptoCarousel</h2>
+    <router-link to="/">
+      <h3>Home</h3>
+    </router-link>
+    <router-link to="/Address">
+      <h3>Address</h3>
+    </router-link>
+    <router-link to="/About">
+      <h3>About</h3>
+    </router-link>
   </div>
 </template>
 
@@ -16,11 +21,42 @@ export default {
 </script>
 
 <style scoped>
+
   .topnav{
     display: flex;
+    align-content: center;
+    flex-flow: row wrap;
+    justify-content: space-between;
     height: 5vh;
     background-color: rgb(255, 255, 255);
     box-shadow:0 10px 20px -10px #cdcdcd;
-    padding: 2%;
+    height: 6em;
+    padding: 0 2%;
+}
+
+h2, a {
+  display: flex;
+  flex-flow: row wrap;
+  align-content: center;
+  justify-content: center;
+}
+
+a {
+  text-decoration: none;
+  color: rgb(0, 0, 0);
+  width: 8em;
+  height: 2.5em;
+  border: 2px solid rgb(255, 255, 255);
+  border-radius: 10px;
+  transition: 0.3s;
+}
+
+a:hover {
+  transition: 0.5s;
+  border-color: black;
+}
+
+a.router-link-exact-active {
+  border-color: black;
 }
 </style>
