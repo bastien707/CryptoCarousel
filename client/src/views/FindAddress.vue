22e9bf827256fdf3bf19df8202c3a000c35f93fa
bastien707 <bastien.facqueur@efrei.net>
2022-05-02 15:15:31 -0700

    style improved

--- client/src/views/FindAddress.vue
+++ client/src/views/FindAddress.vue
@@ -2,6 +2,7 @@
     <TopNav />
     <div class="content">
         <h1>Browse through the Blockchain</h1>
+        <span>Enter an address below to find more info about this wallet</span>
         <PostAddress />
     </div>
     <FooterView />
@@ -30,44 +31,29 @@ body {
     margin: 0;
 }
 
-h1 {
-    font-size: 56px;
-    max-width: 550px;
-    margin: auto;
-    font-weight: 800;
-}
-
-.input{
-    border-width: 1px;
-    border-color: rgb(210, 210, 210);
-    padding: 10px;
-    border-radius: 10px;
-    margin: 10px;
-    min-width: 400px;
-}
-
-.form{
-    background-color: rgb(255, 255, 255);
+.content {
     display: flex;
-    align-items: center;
-    flex-direction: column;
-    border-radius: 20px;
-    margin-top: 10%;
-    padding: 8%;
-    border-style: solid;
-    border-width: 1px;
-    border-color: rgb(215, 215, 215);
-    box-shadow:0 10px 20px -10px #9e9e9e;
+    align-content: center;
+    justify-content: center;
+    flex-flow: column wrap;
+    min-height: 40vh;
 }
 
-.submit-ref {
-    background-image: linear-gradient(45deg, #61f7ff 0%, #33b1ff 100%);
-    padding-top: 10px;
-    padding-bottom: 10px;
-    padding-left: 40px;
-    padding-right: 40px;
-    border-radius: 14px;
-    box-shadow:0 10px 20px -10px #33c9f3;
-    transition: 0.4s;
+span {
+    color: grey;
+    font-size: 2em;
+    font-weight: 300;
+    line-height: 30px;
+    text-align: center;
+    margin: 1em;
+}
+
+h1 {
+    text-align: center;
+    font-size: 56px;
+    max-width: 8em;
+    font-weight: 800;
+    margin: auto;
+    padding: 4%;
 }
 </style>
