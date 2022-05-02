628febbc672d1822aa93379e3d4103ff776bd011
Oscar <oscar.vilmot@gmail.com>
2022-05-01 22:57:04 -0700

    Knex implement for assets

--- app.js
+++ app.js
@@ -1,15 +1,37 @@
-const express = require('express')
-const path = require('path')
-const port = 3000;
-const app = express()
-const test = require('./modules/query')
+const dotenv = require('dotenv')
+dotenv.config()
 
-app.use(express.static(__dirname + '/public'))
+const express = require('express');
+const app = express()
+
+const port = process.env.PORT || 3000;
+
+const pg_updater = require('./db/update_pg')
+const pg_creater = require('./db/create_pg')
+
+const dev_ws = require('./modules/query')
+
+pg_updater.update_assets_price()
+
+setInterval(async () => {
+    // update DB on each call
+    await pg_creater.create_assets()
+    await pg_updater.update_assets()
+}, 10*1000)
 
 app.get('/', (req, res) => {
-    let val = test.getBitcoinPrice()
-    res.send(val)
-    /* res.render(path.join(__dirname, 'views/index.pug')) */
+    // main menu containing the most relevant informations about the actual state of the blockchain
+    res.send()
+})
+
+app.get('/card/:crypto', (req, res) => {
+    // send a card with the informations of a cryptocurrency
+    res.send()
+})
+
+app.get('/dev/:val', (req, res) =>{
+    // development endpoint
+    res.send()
 })
 
 // go to localhost:3000
--- modules/query.js
+++ modules/query.js
@@ -1,9 +1,10 @@
 const WebSocket = require('ws')
 
-const webPrice = new WebSocket("wss://ws.coincap.io/trades/binance")
+const webPrice = new WebSocket("wss://ws.coincap.io/prices?assets=bitcoin")
 
 exports.getBitcoinPrice = () => {
     webPrice.onmessage = function (msg){
         console.log(msg.data)
     }
-}
+} 
+
